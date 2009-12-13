1e313ab4d091dd3ded0fd5b173626325439157c8
Corbin Simpson <simpsoco@osuosl.org>
2009-12-13 13:07:05 -0800

    shesaid: Regexp instead of strstr, and improve matching.
    This improves response time and accuracy.
    
    (That's what she said.)

--- lib/python/hackabot/plugins/shesaid.py
+++ lib/python/hackabot/plugins/shesaid.py
@@ -4,23 +4,26 @@ The eternal retort, or at least this decade's version of it.
 ~ Corbin Simpson <simpsoco@osuosl.org>
 """
 
+import re
+
 from zope.interface import implements
 from twisted.plugin import IPlugin
 from hackabot.plugin import IHackabotPlugin
 
-class Ponies(object):
+class SheSaid(object):
     implements(IPlugin, IHackabotPlugin)
 
-    @staticmethod
-    def msg(conn, event):
+    def __init__(self):
+        self.regex = re.compile("(?:^|\s)(?:it in|need(?:ed)? it)", re.I)
+
+    def msg(self, conn, event):
         if event["sent_by"] == conn.nickname:
             return
 
-
-        if "it in" in event["text"].lower():
+        if self.regex.search(event["text"]):
             conn.msg(event["reply_to"], "%s: That's what she said." %
                 event["sent_by"])
 
     me = msg
 
-ponies = Ponies()
+shesaid = SheSaid()
